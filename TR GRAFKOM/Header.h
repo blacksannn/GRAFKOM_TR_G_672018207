028d958c60bd53118c86077531756a2b82938120
blacksannn <drewwwww.99@gmail.com>
2020-08-05 06:11:43 -0800

    Progress 2 TR

--- TR GRAFKOM/Header.h
+++ TR GRAFKOM/Header.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 GLuint loadBMP_custom(const char* imagepath) {
-	unsigned char header[54];
+	unsigned char Header[54];
 	unsigned int dataPos;
 	unsigned int width, height;
 	unsigned int imageSize;
@@ -12,28 +12,28 @@ GLuint loadBMP_custom(const char* imagepath) {
 	fopen_s(&file, imagepath, "rb");
 	if (!file) { printf("Image could not be opened\n"); return 0; }
 
-	if (fread(header, 1, 54, file) != 54) {
+	if (fread(Header, 1, 54, file) != 54) {
 		printf("Not a correct BMP file\n");
 		fclose(file);
 		return 0;
 	}
-	if (header[0] != 'B' || header[1] != 'M') {
+	if (Header[0] != 'B' || Header[1] != 'M') {
 		printf("Not a correct BMP file\n");
 		fclose(file);
 		return 0;
 	}
 
-	if (*(int*)&(header[0x1E]) != 0) { printf("Not a correct BMP file\n"); return 0; }
-	if (*(int*)&(header[0x1C]) != 24) { printf("Not a correct BMP file\n"); return 0; }
-	if (header[0] != 'B' || header[1] != 'M') {
+	if (*(int*)&(Header[0x1E]) != 0) { printf("Not a correct BMP file\n"); return 0; }
+	if (*(int*)&(Header[0x1C]) != 24) { printf("Not a correct BMP file\n"); return 0; }
+	if (Header[0] != 'B' || Header[1] != 'M') {
 		printf("Not a correct BMP file\n");
 		fclose(file);
 	}
 
-	dataPos = *(int*)&(header[0x0A]);
-	imageSize = *(int*)&(header[0x22]);
-	width = *(int*)&(header[0x12]);
-	height = *(int*)&(header[0x16]);
+	dataPos = *(int*)&(Header[0x0A]);
+	imageSize = *(int*)&(Header[0x22]);
+	width = *(int*)&(Header[0x12]);
+	height = *(int*)&(Header[0x16]);
 
 	if (imageSize == 0) imageSize = width * height * 3;
 	if (dataPos == 0)dataPos = 54;
--- TR GRAFKOM/TR GRAFKOM.cpp
+++ TR GRAFKOM/TR GRAFKOM.cpp
@@ -1,7 +1,10 @@
-#include <GL/freeglut.h>
-#include "Header.h"
+#include <GL/freeglut.h>\
+#include <window.s>
+#include "header.h"
+
 
 GLuint _textureID;
+float angle = 0.0f;
 
 void drawCube() {
 	glEnable(GL_TEXTURE_2D);
@@ -12,51 +15,15 @@ void drawCube() {
 	glColor3f(1.0f, 1.0f, 1.0f);
 	glBegin(GL_POLYGON);
 	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(-0.5, 0.5, -0.5);
 	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(0.5, 0.5, -0.5);
 	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(0.5, -0.5, -0.5);
 	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-0.5, -0.5, -0.5);
 
 	glEnd();
-	/*
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glBegin(GL_POLYGON);
-	glVertex3f(-0.5f, 0.5f, -0.5f);
-	glVertex3f(0.5f, 0.5f, -0.5f);
-	glVertex3f(0.5f, -0.5f, -0.5f);
-	glVertex3f(-0.5f, -0.5f, -0.5f);
-	glEnd();
-
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glBegin(GL_POLYGON);
-	glVertex3f(0.5f, 0.5f, 0.5f);
-	glVertex3f(0.5f, 0.5f, -0.5f);
-	glVertex3f(0.5f, -0.5f, -0.5f);
-	glVertex3f(0.5f, -0.5f, 0.5f);
-	glEnd();
-
-	glColor3f(1.0f, 1.0f, 0.0f);
-	glBegin(GL_POLYGON);
-	glVertex3f(-0.5f, 0.5f, 0.5f);
-	glVertex3f(-0.5f, 0.5f, -0.5f);
-	glVertex3f(-0.5f, -0.5f, -0.5f);
-	glVertex3f(-0.5f, -0.5f, 0.5f);
-	glEnd();
-
-	glColor3f(0.0f, 1.0f, 1.0f);
-	glBegin(GL_POLYGON);
-	glVertex3f(-0.5f, 0.5f, 0.5f);
-	glVertex3f(0.5f, 0.5f, 0.5f);
-	glVertex3f(0.5f, 0.5f, -0.5f);
-	glVertex3f(-0.5f, 0.5f, -0.5f);
-	glEnd();
-
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glBegin(GL_POLYGON);
-	glVertex3f(-0.5f, -0.5f, 0.5f);
-	glVertex3f(0.5f, -0.5f, 0.5f);
-	glVertex3f(0.5f, -0.5f, -0.5f);
-	glVertex3f(0.5f, -0.5f, -0.5f);
-	glEnd();*/
 
 	glDisable(GL_TEXTURE_2D);
 }
@@ -66,6 +33,7 @@ void display() {
 	glEnable(GL_DEPTH_TEST);
 
 	glPushMatrix();
+	glRotatef(angle, 0.0, 1.0, 0.0);
 	drawCube();
 	glPopMatrix();
 
@@ -74,12 +42,105 @@ void display() {
 
 void myinit() {
 	glClearColor(1.0, 1.0, 1.0, 1.0);
-	_textureID = loadBMP_custom("C:/Users/Andrew Nussy/source/repos/TR GRAFKOM/TR GRAFKOM/Woman1.nfg");
+	_textureID = loadBMP_custom("C:/Users/Andrew Nussy/source/repos/TR GRAFKOM/TR GRAFKOM/Woman12.bmp");
 }
 
 void timer(int) {
 	glutPostRedisplay();
 	glutTimerFunc(1000 / 30, timer, 0);
+	angle += 1.0f;
+
+}
+
+void keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+
+	case 'j':
+		glRotatef(2.0, 1.0, 0.0, 0.0);
+		glutPostRedisplay();
+		break;
+
+	case 'u':
+		glRotatef(-2.0, 1.0, 0.0, 0.0);
+		glutPostRedisplay();
+		break;
+
+	case 'i':
+		glRotatef(2.0, 0.0, 1.0, 0.0);
+		glutPostRedisplay();
+		break;
+
+	case 'y':
+		glRotatef(-2.0, 0.0, 1.0, 0.0);
+		glutPostRedisplay();
+		break;
+
+	case 'n':
+		glRotatef(2.0, 0.0, 0.0, 1.0);
+		glutPostRedisplay();
+		break;
+
+	case 'm':
+		glRotatef(-2.0, 0.0, 0.0, 1.0);
+		glutPostRedisplay();
+		break;
+
+	case 'r':
+		glTranslatef(3.0, 0.0, 0.0);
+		glutPostRedisplay();
+		break;
+
+	case 'w':
+		glTranslatef(-3.0, 0.0, 0.0);
+		glutPostRedisplay();
+		break;
+
+	case 'e':
+		glTranslatef(0.0, 3.0, 0.0);
+		glutPostRedisplay();
+		break;
+
+	case 'd':
+		glTranslatef(0.0, -3.0, 0.0);
+		glutPostRedisplay();
+		break;
+
+	case 'x':
+		if (_textureID) {
+			_textureID = 0;
+			glDisable(GL_DEPTH_TEST);
+		}
+		else {
+			_textureID = 1;
+			glEnable(GL_DEPTH_TEST);
+		}
+	}
+}
+
+void key(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_PAGE_UP:
+		glTranslatef(0.0, 0.0, 0.1);
+		glutPostRedisplay();
+		
+		break;
+
+	case GLUT_KEY_PAGE_DOWN:
+		glTranslatef(0.0, 0.0, -0.1);
+		glutPostRedisplay();
+		break;
+	}
+
+}
+
+void ukuran(int lebar, int tinggi) {
+	if (tinggi == 0) tinggi = 1;
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(100.0, lebar / tinggi, 5.0, 500.0);
+	glTranslatef(0.0, -5.0, -150.0);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char** argv) {
@@ -89,9 +150,10 @@ int main(int argc, char** argv) {
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Texturing");
 	glutDisplayFunc(display);
-
 	myinit();
-
+	//glutKeyboardFunc(keyboard);
+	//glutSpecialFunc(key);
+	//glutReshapeFunc(ukuran);
 	glutTimerFunc(0, timer, 0);
 	glutMainLoop();
 
